72347de4f55d827a46690420c1ff56846d174341
ab-navarra <ab@navarra-lab.com>
2022-10-25 14:20:14 +0200

    Lab4

---
+++ src/main/kotlin/lab4/ex2.kt
@@ -0,0 +1,45 @@
+package lab4
+
+fun main() {
+    val studentStatus: (String, String) -> Unit = { name: String, aura: String ->
+        println("$name has a $aura face color\n")
+    }
+
+    studentStatus("KEKW", "red")
+
+    val student1 = StudentNoConst("lol")
+    println(student1.name)
+    println()
+
+    val student2 = Student("Kekw")
+    student2.showStudent()
+
+    val student3 = StudentPerson("stu", "dent")
+    student3.showStudent()
+}
+
+class StudentNoConst(name: String) {
+    val name: String = name
+        get() = field.capitalize()
+}
+
+class Student(private val name: String, private val university: String = "pk") {
+    val showStudent: () -> Unit = {
+        println("$name studies at $university\n")
+    }
+}
+
+open class Person(name: String) {
+    var name: String
+
+    init {
+        this.name = name.capitalize()
+    }
+}
+
+class StudentPerson(name: String, private var university: String) : Person(name) {
+    val showStudent: () -> Unit = {
+        val localName = super.name
+        println("$localName studies at $university\n")
+    }
+}
